bcf5563f54d466e1ac5404a92f81b7cbe4ce92b1
haixinchen <haixinchen@tencent.com>
2021-01-18 01:46:02 +0800

    feat(webhdfs): IsXXXException

--- api.error.go
+++ api.error.go
@@ -11,3 +11,51 @@ func (e ErrorResponse) Exception() error {
 	}
 	return e.RemoteException
 }
+
+func IsIllegalArgumentException(err error) bool {
+	except, ok := err.(*RemoteException)
+	if !ok {
+		return false
+	}
+	if except.Exception == "IllegalArgumentException" {
+		return true
+	}
+
+	return false
+}
+
+func IsSecurityException(err error) bool {
+	except, ok := err.(*RemoteException)
+	if !ok {
+		return false
+	}
+	if except.Exception == "SecurityException" {
+		return true
+	}
+
+	return false
+}
+
+func IsAccessControlException(err error) bool {
+	except, ok := err.(*RemoteException)
+	if !ok {
+		return false
+	}
+	if except.Exception == "AccessControlException" {
+		return true
+	}
+
+	return false
+}
+
+func IsFileNotFoundException(err error) bool {
+	except, ok := err.(*RemoteException)
+	if !ok {
+		return false
+	}
+	if except.Exception == "FileNotFoundException" {
+		return true
+	}
+
+	return false
+}
